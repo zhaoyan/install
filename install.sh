19524b61600cdf3d0f78202c4b521d296064c1e0
yan.zhao <yan.zhao@metecs.com>
2018-04-30 15:49:15 -0500

    new fix

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 sudo add-apt-repository ppa:jonathonf/vim
 sudo apt update
 sudo apt  install vim
+
+sudo apt-get -y update
+sudo apt-get -y install git-core
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 if [  -f ~/.vimrc ]; then
@@ -13,8 +16,7 @@ cp ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
 vim +PluginInstall +qall
 cp -R after ~/.vim/after/
 cp -R plugin ~/.vim/plugin/
-cp -R macros ~/.vim/macros/
-
+cp -R macros ~/.vim/macros/ 
 
 
 #install powerline font
@@ -34,15 +36,14 @@ rm -rf fonts
  sudo apt-get -y install dconf-tools
  dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:swapescape']"
 
- # install double comd
-sudo add-apt-repository ppa:alexx2000/doublecmd 
-sudo apt-get update
-sudo apt-get -y install doublecmd-gtk 
-if [  -f ~/.config/doublecmd/doublecmd.xml ]; then
-		mv ~/.config/doublecmd/doublecmd.xml ~/.config/doublecmd/doublecmd_copy.xml 
-fi 
-cp doublecmd.xml ~/.config/doublecmd/doublecmd.xml
+#install chrome 
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main"  | sudo tee --append /etc/apt/sources.list
+wget https://dl.google.com/linux/linux_signing_key.pub 
+sudo apt-key add linux_signing_key.pub
+sudo apt update
+sudo apt install google-chrome-stable
 
+#install YCM
 
 sudo apt-get -y install build-essential cmake
 sudo apt-get -y install python-dev python3-dev
@@ -51,8 +52,17 @@ cd ~/.vim/bundle/YouCompleteMe
 ./install.py --clang-completer
 cd -
 
-echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main"  | sudo tee --append /etc/apt/sources.list
-wget https://dl.google.com/linux/linux_signing_key.pub 
-sudo apt-key add linux_signing_key.pub
-sudo apt update
-sudo apt install google-chrome-stable
+ # install double comd
+sudo add-apt-repository ppa:alexx2000/doublecmd 
+sudo apt-get update
+sudo apt-get -y install doublecmd-gtk 
+
+doublecmd
+echo "Now copy doublecmd file"
+sleep 5
+if [  -f ~/.config/doublecmd/doublecmd.xml ]; then
+		mv ~/.config/doublecmd/doublecmd.xml ~/.config/doublecmd/doublecmd_copy.xml 
+fi 
+cp doublecmd.xml ~/.config/doublecmd/doublecmd.xml
+
+
