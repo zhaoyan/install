06c1fba992f4b872f2fecbc521839440b57e8b54
Yan Zhao <zhaoyan.hrb@gmail.com>
2018-05-30 23:54:14 -0500

    add git and ssh support

---
+++ git_ssh.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ssh-keygen -t rsa -b 4096 -C "zhaoyan.hrb@gmail.com"
+sudo apt-get install xclip
+# Downloads and installs xclip. If you don't have `apt-get`, you might need to use another installer (like `yum`)
+xclip -sel clip < ~/.ssh/id_rsa.pub
+# Copies the contents of the id_rsa.pub file to your clipboard
+
+cp config ~/.ssh/config 
